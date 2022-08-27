bd9b722bab8a0c6a82776809f163b4586aad37a3
for-demo <oneliang0212@gmail.com>
2022-08-27 23:10:48 +0800

    init

---
+++ index.js
@@ -0,0 +1,92 @@
+const splide = new Splide("#splide", {
+  type: "fade",
+  rewind: true,
+  pagination: false,
+  arrows: false,
+  autoplay: true,
+  interval: 4000,
+});
+
+splide.mount();
+
+const bodyClass = document.querySelector("body").classList;
+const header = document.querySelector("#header");
+const headerClass = document.querySelector("#header").classList;
+const memuButton = document.querySelector("#memuButton");
+const menuIconClass = document.querySelector("#menuIcon").classList;
+const navbarClass = document.querySelector("#navbar").classList;
+const scrollButton = document.querySelector("#scrollButton");
+const about = document.querySelector("#about");
+
+window.addEventListener("scroll", function () {
+  if (document.documentElement.scrollTop > 0) {
+    header.classList.add("header-scrolled");
+  } else {
+    header.classList.remove("header-scrolled");
+  }
+});
+
+memuButton.addEventListener("click", function () {
+  bodyClass.toggle("overflow-hidden");
+  headerClass.toggle("header-active");
+  menuIconClass.toggle("menuIcon-active");
+  navbarClass.toggle("navbar-active");
+});
+
+scrollButton.addEventListener("click", function () {
+  about.scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+  });
+});
+
+// gotop
+if (
+  window.requestAnimationFrame ||
+  window.mozRequestAnimationFrame ||
+  window.webkitRequestAnimationFrame ||
+  window.msRequestAnimationFrame
+) {
+  let requestAnimationFrame =
+    window.requestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.msRequestAnimationFrame;
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  const goTopButton = document.querySelector("#gotop");
+  const windowViewPortHeight = window.innerHeight;
+  let isRequestingAnimationFrame = false;
+
+  if (!goTopButton) {
+    return;
+  }
+
+  goTopButton.addEventListener("click", function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+
+  window.addEventListener("scroll", function () {
+    if (!isRequestingAnimationFrame) {
+      requestAnimationFrame(filterGoTopButtonVisibility);
+      isRequestingAnimationFrame = true;
+    }
+  });
+
+  function filterGoTopButtonVisibility() {
+    let windowPageYOffset =
+      window.pageYOffset || document.documentElement.scrollTop;
+    if (windowPageYOffset > windowViewPortHeight) {
+      goTopButton.classList.add("show");
+      isRequestingAnimationFrame = false;
+    } else {
+      goTopButton.classList.remove("show");
+      requestAnimationFrame(filterGoTopButtonVisibility);
+    }
+  }
+});
+// gotop end
